f272818e29faa6968b6ada420f8d9b53ecf1ee5d
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-05-01 17:34:08 +0300

    Fix token validation

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 	pb "github.com/soypita/go-shipping/proto/consignment"
+	userService "github.com/soypita/shippy-service-user/proto/user"
 	vesselProto "github.com/soypita/shippy-service-vessel/proto/vessel"
 )
 
@@ -15,10 +20,38 @@ const (
 	defaultHost = "datastore:27017"
 )
 
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, resp interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in request")
+		}
+
+		token, ok := meta["Token"]
+		if !ok {
+			return errors.New("no token in meta")
+		}
+
+		log.Println("Authenticating with token: ", token)
+
+		// Auth here
+		authClient := userService.NewUserServiceClient("go.micro.srv.user", client.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &userService.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, resp)
+		return err
+	}
+}
+
 func main() {
 	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
 		micro.Name("shippy.consignment.service"),
+		micro.WrapHandler(AuthWrapper),
 	)
 
 	// Init will parse the command line flags.
